f7e63efb9b3a952682414a78420dae6eaaf757ca
gabriel <ggabrieldeveloper@gmail.com>
2023-02-10 19:07:13 -0300

    add devServer config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -78,4 +78,11 @@ module.exports = {
     }),
     new Dotenv(),
   ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true,
+  },
 }
